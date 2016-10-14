bcd1636962f2448d57868eca87455406c6865cbc
Amr Ayoub <Amr Ayoub>
2016-10-14 17:31:32 +0300

    Adding circle and triangle functions

--- mindstorm.py
+++ mindstorm.py
@@ -2,26 +2,33 @@
 
 import turtle
 
-def square():
+window = turtle.Screen()
+window.bgcolor("red")
+brad = turtle.Turtle()
 
-    window = turtle.Screen()
-    window.bgcolor("red")
     
-    brad = turtle.Turtle()
+def square():
+    for x in range(0,4):
+        brad.forward(100)
+        brad.right(90)
 
 
+
+def circle():
+    brad.circle(100,None,None)
+    
+    
+def triangle():
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    brad.right(135)
+    brad.forward(142)
 
-
-    window.exitonclick()
 
 
 square()
+circle()
+triangle()
+window.exitonclick()
 
