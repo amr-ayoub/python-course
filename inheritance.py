6bca08491fcc0694899baedbf878669593f42234
Amr Ayoub <Amr Ayoub>
2016-10-16 18:37:59 +0300

    Modify inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -3,9 +3,31 @@ class Parent():
         self.first = first
         self.last = last
 
+    def show_info(self):
+        print(self.first)
+        print(self.last)
+    
 
+
+
+class Child(Parent):
+    def __init__(self,first,last,toys):
+        Parent.__init__(self,first,last)
+        self.toys = toys
+
+    def show_info(self):
+        #Parent.show_info(self)
+        print(self.toys)
+    
 
 
 Amr = Parent("Amr","Ayoub")
-print(Amr.first)
+#Amr.show_info()
 
+
+#print(Amr.first)
+Yahia = Child("Yahia","Ayoub",3)
+Yahia.show_info()
+
+#print(Yahia.last)
+#print(Yahia.toys)
